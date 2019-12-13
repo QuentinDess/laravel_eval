8948f38cda30381a08c99bc9b47aa1b0003dbebb
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-12-13 16:26:06 +0100

    rectif

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,16 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/showAnimals','AnimalController@show')->name('showAnimals');
 ROUTE::group(['middleware'=>['auth']], function(){
-   
-
-    Route::get('/showRaces','RaceController@show')->name('showRaces');
-    
-});
-ROUTE::group(['middleware'=>['auth','CheckAdmin']], function(){
-
-    Route::get('/createRace', 'RaceController@create')->name('createRace');
-    ROUTE::post('/storeRace', 'RaceController@store')->name('storeRace');
-    ROUTE::post('/editRace/{id}', 'RaceController@edit')->name('editRace');
-    ROUTE::post('/updateRace/{id}', 'RaceController@update')->name('updateRace');
-    ROUTE::post('/deleteRace/{id}', 'RaceController@destroy')->name('deleteRace');
     Route::get('/createAnimal', 'AnimalController@create')->name('createAnimal');
     ROUTE::post('/storeAnimal', 'AnimalController@store')->name('storeAnimal');
     ROUTE::post('/editAnimal/{id}', 'AnimalController@edit')->name('editAnimal');
     ROUTE::post('/updateAnimal/{id}', 'AnimalController@update')->name('updateAnimal');
     ROUTE::post('/deleteAnimal/{id}', 'AnimalController@destroy')->name('deleteAnimal');
+
+    Route::get('/showRaces','RaceController@show')->name('showRaces');
+    Route::get('/createRace', 'RaceController@create')->name('createRace');
+    ROUTE::post('/storeRace', 'RaceController@store')->name('storeRace');
+    ROUTE::post('/editRace/{id}', 'RaceController@edit')->name('editRace');
+    ROUTE::post('/updateRace/{id}', 'RaceController@update')->name('updateRace');
+    ROUTE::post('/deleteRace/{id}', 'RaceController@destroy')->name('deleteRace');
 });
