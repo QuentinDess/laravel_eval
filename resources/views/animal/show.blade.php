df30ea8bba38344385509678a97f1cc4b3882aca
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-12-13 16:25:01 +0100

    rectif

--- resources/views/animal/show.blade.php
+++ resources/views/animal/show.blade.php
@@ -4,7 +4,7 @@
 <table class="table table-striped table-bordered">
    
     <tr class="thead-dark">
-       
+        @if(Auth::check())
         @if(Auth::user()->role->role=="Admin")
         <th scope="col">#</th>
         <th scope="col">Nom</th>
@@ -22,6 +22,14 @@
         <th scope="col">Taille</th>
         <th scope="col">Race</th>
         @endif
+        @else 
+        <th scope="col">#</th>
+        <th scope="col">Nom</th>
+        <th scope="col">Age</th>
+        <th scope="col">Genre</th>
+        <th scope="col">Taille</th>
+        <th scope="col">Race</th>
+        @endif
 
     </tr>
 
@@ -29,6 +37,7 @@
 @foreach ($animals as $animal )
     
     <tr>
+        @if(Auth::check())
         @if(Auth::user()->role->role=="Admin")
         <td class="alert-warning"> {{$animal->id}} </td>
         <td class="alert-info"> {{$animal->name}} </td>
@@ -50,6 +59,14 @@
             <button type="submit" class="btn-primary">Supprimer</button>
         </form>
         </td>
+        @else 
+        <td class="alert-warning"> {{$animal->id}} </td>
+        <td class="alert-info"> {{$animal->name}} </td>
+        <td class="alert-info"> {{$animal->age}} </td>
+        <td class="alert-info"> {{$animal->gender}} </td>
+        <td class="alert-info"> {{$animal->size}}cm </td>
+        <td class="alert-warning"> {{$animal->race->name}} </td>
+        @endif
         @else 
         <td class="alert-warning"> {{$animal->id}} </td>
         <td class="alert-info"> {{$animal->name}} </td>
