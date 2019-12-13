6e0145a606615179ba703c79ee04510e6afb79c3
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-12-13 16:25:32 +0100

    rectif

--- resources/views/race/show.blade.php
+++ resources/views/race/show.blade.php
@@ -6,7 +6,7 @@
 <table class="table table-striped table-bordered">
    
     <tr class="thead-dark">
-       
+        @if(Auth::check())
         @if(Auth::user()->role->role=="Admin")
         <th scope="col">#</th>
         <th scope="col">Nom</th>
@@ -20,12 +20,19 @@
         <th scope="col">Classification</th>
         <th scope="col">Espérance de vie</th>
         @endif
+        @else 
+        <th scope="col">#</th>
+        <th scope="col">Nom</th>
+        <th scope="col">Classification</th>
+        <th scope="col">Espérance de vie</th>
+        @endif
     </tr>
 
 
 @foreach ($races as $race )
     
     <tr>
+        @if(Auth::check())
         @if(Auth::user()->role->role=="Admin")
         <td class="alert-warning"> {{$race->id}} </td>
         <td class="alert-info"> {{$race->name}} </td>
@@ -45,6 +52,12 @@
             <button type="submit" class="btn-primary">Supprimer</button>
         </form>
         </td>
+        @else 
+        <td class="alert-warning"> {{$race->id}} </td>
+        <td class="alert-info"> {{$race->name}} </td>
+        <td class="alert-info"> {{$race->classification}} </td>
+        <td class="alert-info"> {{$race->life}} </td>
+        @endif
         @else 
         <td class="alert-warning"> {{$race->id}} </td>
         <td class="alert-info"> {{$race->name}} </td>
